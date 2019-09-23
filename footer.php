21d9ef76c04e75665bd7a0adae5bbb8659a0d47f
Dung Nguyen <nguyendung622@Dungs-MacBook-Air.local>
2019-09-23 10:26:56 +0700

    Ngày 23/09/2019

--- footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <p>Copyright: ndungithue</p>
-</footer>
--- nav.php
+++
@@ -1,6 +0,0 @@
-<ul>
-    <li><a href="index.php">Trang chủ</a></li>
-    <li><a href="tinhtoancoban.php">Tính toán cơ bản</a></li>
-    <li><a href="studentinformation.php">Thông tin sinh viên</a></li>
-    <li><a href="login.php">Login</a></li>
-</ul>
